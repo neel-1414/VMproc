6a0f6f2aeb7e651c4fb40c0025272c2f68be3a68
neel-1414 <neelyshinde1@gmail.com>
2025-12-24 23:52:39 +0530

    added the identifiers

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,9 +1,34 @@
 
+import java.util.HashMap;
+
+
 public class Scanner {
     private  int line = 1;
-   private  int start =0;
+    private  int start =0;
     private  int current =0;
     String source;
+    HashMap<String , TokenType> identify = new HashMap<>();
+    public Scanner()
+    {
+        identify.put("and",    TokenType.TOKEN_AND);
+        identify.put("class",  TokenType.TOKEN_CLASS);
+        identify.put("else",   TokenType.TOKEN_ELSE);
+        identify.put("false",  TokenType.TOKEN_FALSE);
+        identify.put("for",    TokenType.TOKEN_FOR);
+        identify.put("fun",    TokenType.TOKEN_FUN);
+        identify.put("if",     TokenType.TOKEN_IF);
+        identify.put("nil",    TokenType.TOKEN_NIL);
+        identify.put("or",     TokenType.TOKEN_OR);
+        identify.put("print",  TokenType.TOKEN_PRINT);
+        identify.put("return", TokenType.TOKEN_RETURN);
+        identify.put("super",  TokenType.TOKEN_SUPER);
+        identify.put("this",   TokenType.TOKEN_THIS);
+        identify.put("true",   TokenType.TOKEN_TRUE);
+        identify.put("var",    TokenType.TOKEN_VAR);
+        identify.put("while",  TokenType.TOKEN_WHILE);
+
+    }
+
     public Scanner(String source)
     {
         this.source = source;
@@ -152,8 +177,8 @@ public class Scanner {
         {
             advance();
             while(isDigit(peek())) advance();
-
         }
+
         return makeToken(TokenType.TOKEN_NUMBER, Integer.valueOf(source.substring(start+1, current-1)) ,source.substring(start,current));
     }
     
@@ -165,11 +190,12 @@ public class Scanner {
     {
         while(isAlpha(peek()) || isDigit(peek())) advance();
 
-        return makeToken(indentiferType(), source.substring(start+1, current-1), source.substring(start,current));
+        return makeToken(identifierType(source.substring(start+1, current-1)), source.substring(start+1, current-1), source.substring(start,current));
     }
-    private TokenType identifierType()
+    private TokenType identifierType(String s)
     {
-        //continue 289
+       if(identify.get(s) == null) return  TokenType.TOKEN_IDENTIFIER;
+       return identify.get(s);
     }
 }
 
